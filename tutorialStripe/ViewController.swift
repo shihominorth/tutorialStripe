9626729348004f45586d42d81445930702b2859b
Shihomi Kitajima <shihomi0104@icloud.com>
2020-06-27 03:35:21 -0700

    Initial Commit

---
+++ tutorialStripe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tutorialStripe
+//
+//  Created by 北島　志帆美 on 2020-06-27.
+//  Copyright © 2020 北島　志帆美. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
